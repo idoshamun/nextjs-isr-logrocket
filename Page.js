b2ca608d496febe2d67a8cac9e37a4f219cb20b6
Ido Shamun <idoesh1@gmail.com>
2020-12-30 16:25:41 +0200

    Support fallback mode in ISR

--- Page.js
+++ Page.js
@@ -1,4 +1,12 @@
+import { useRouter } from 'next/router';
+
 export default function Page(props) {
+    const { isFallback } = useRouter();
+
+    if (isFallback) {
+        return <></>;
+    }
+
     return <div>
         <h1>{props.name}</h1>
         <p>{props.description}</p>
